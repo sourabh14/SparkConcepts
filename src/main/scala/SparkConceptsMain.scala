c1962de018da6b49c6f2583716c385872ba722e8
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-09-26 19:44:17 +0530

    Initial commit

---
+++ src/main/scala/SparkConceptsMain.scala
@@ -0,0 +1,11 @@
+@main def SparkConceptsMain() =
+    /*
+      Spark Introduction
+        SBT command - on project directory
+            - sbt compile
+            - sbt package
+            - sbt run  (to build and run)
+            - sbt (this will open REPL)
+    */
+
+    println("---- Spark Demo Project ----")
